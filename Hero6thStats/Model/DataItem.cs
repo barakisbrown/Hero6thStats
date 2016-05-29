d36b37d9830a29fa542085788d3d007e0ddeace6
Matthew Brown <barakis@barakisbrown.com>
2016-05-28 19:34:15 -0500

    Add project files.

---
+++ Hero6thStats/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Hero6thStats.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
